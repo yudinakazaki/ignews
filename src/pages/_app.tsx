f47393e1af280499e220fb71550165ed4cf53fea
Yudi Nakazaki <yudinakazaki@gmail.com>
2022-10-06 19:09:56 -0300

    feat: adding github authentication with OAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { Header } from '../components/Header'
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
